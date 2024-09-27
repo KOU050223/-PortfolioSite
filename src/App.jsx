d1e4b5441a2aee86d116194ebb769638764a84fb
KOU050223 <kouhei20050223@icloud.com>
2024-09-28 03:56:19 +0900

    [add]作品一覧雛型完成

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,112 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>KOUのこれまでの作品一覧</h1>
+        <table>
+        <tr>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">DUB DESiGN</p>
+              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+            </div>
+            <div class="card-link">
+              <a href="#">HOME</a>
+              <a href="#">FAQ</a>
+              <a href="#">PRIVACY</a>
+            </div>
+          </div>
+          </div>
+          </td>
+        </tr>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
