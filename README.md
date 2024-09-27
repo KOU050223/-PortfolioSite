b4e951fae945334b13071f3fe9f0297fc535158d
KOU050223 <kouhei20050223@icloud.com>
2024-09-28 04:51:31 +0900

    作品情報追加

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # KOUのPortfolioサイト作成
+
+## Vire&React作成参考
+
+https://monotein.com/blog/react-vite-how-to-use
+
+## GitHubPagesでぷろい参考
+
+https://monotein.com/blog/react-vite-how-to-use
--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">FK大 RPG</p>
+              <p class="card-text">KOU初作品<br />立花祭に出店したい一心で１週間ほどで作成したゲームバランスが終わっているゲーム</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="#">GitHub</a>
+              <a href="#">リンク</a>
+              <a href="#">リンク２</a>
             </div>
           </div>
           </div>
@@ -33,13 +33,13 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">ウイルスバスター</p>
+              <p class="card-text">Unityのネコ本を履修後初作品<br />クリックでウイルスを消そう！</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="#">GitHub</a>
+              <a href="#">りんく</a>
+              <a href="#">りんく２</a>
             </div>
           </div>
           </div>
@@ -51,13 +51,13 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">生き残れ！クリオネちゃん</p>
+              <p class="card-text">Unity２作目（１年生２月ごろ）<br />フラッピーバードをつくろう的なやつ丸パクリ</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="#">GitHub</a>
+              <a href="#">りんく</a>
+              <a href="#">りんく２</a>
             </div>
           </div>
           </div>
@@ -67,13 +67,12 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">ポージングエフェクト</p>
+              <p class="card-text">ハックツハッカソン ステゴカップにて作成（2年生5月ごろ）<br />PythonとUnityを繋げて作った。MediaPipeでポーズ推定したよ</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="https://github.com/jyogi-web/sutego_PosingEffect">GitHub</a>
+              <a href="https://topaz.dev/projects/6dd69986efee3f535106">Topaz</a>
             </div>
           </div>
           </div>
@@ -85,13 +84,12 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">人から作る？！錬金クッキング</p>
+              <p class="card-text">第２回福岡学生ゲームジャムにて作成<br />stage1とステージの統合を担当/Unityのチーム開発それぞれのコードを見ないといけなくて大変だった</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="#">GitHubなんてないよ</a>
+              <a href="https://unityroom.com/games/gamejam_2024_08_22_01">UnityRoom</a>
             </div>
           </div>
           </div>
@@ -101,18 +99,52 @@ function App() {
           <div class="card">
             <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
             <div class="card-content">
-              <p class="card-title">DUB DESiGN</p>
-              <p class="card-text">WebデザインやWebサイト制作、最新のWeb業界情報などを紹介していくサイト。<br />しっかりきっかり更新中。</p>
+              <p class="card-title">ジャストエイサー</p>
+              <p class="card-text">第2回やんばるハッカソン＆ハックツハッカソンにて作成<br />Joyconを担当したがバックエンドとの接続や非同期処理難しい</p>
             </div>
             <div class="card-link">
-              <a href="#">HOME</a>
-              <a href="#">FAQ</a>
-              <a href="#">PRIVACY</a>
+              <a href="https://github.com/jyogi-web/yanbaru-back-mediapipe">GitHub(メイン)</a>
+              <a href="https://topaz.dev/projects/3cacf9e968739f90fbe8">Topaz</a>
+              <a href="https://yanabaru-e2dbfbhjdzd0gpg0.japaneast-01.azurewebsites.net/">デプロイ先</a>
             </div>
           </div>
           </div>
           </td>
         </tr>
+        {/* この下に作品を追加 */}
+        {/* <tr>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">人から作る？！錬金クッキング</p>
+              <p class="card-text">第２回福岡学生ゲームジャムにて作成<br />stage1とステージの統合を担当/Unityのチーム開発それぞれのコードを見ないといけなくて大変だった</p>
+            </div>
+            <div class="card-link">
+              <a href="#">GitHubなんてないよ</a>
+              <a href="https://unityroom.com/games/gamejam_2024_08_22_01">UnityRoom</a>
+            </div>
+          </div>
+          </div>
+          </td>
+          <td>
+          <div class="l-wrapper">
+          <div class="card">
+            <img class="card-img" src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg" alt="" />
+            <div class="card-content">
+              <p class="card-title">ジャストエイサー</p>
+              <p class="card-text">第2回やんばるハッカソン＆ハックツハッカソンにて作成<br />Joyconを担当したがバックエンドとの接続や非同期処理難しい</p>
+            </div>
+            <div class="card-link">
+              <a href="https://github.com/jyogi-web/yanbaru-back-mediapipe">GitHub(メイン)</a>
+              <a href="https://topaz.dev/projects/3cacf9e968739f90fbe8">Topaz</a>
+              <a href="https://yanabaru-e2dbfbhjdzd0gpg0.japaneast-01.azurewebsites.net/">デプロイ先</a>
+            </div>
+          </div>
+          </div>
+          </td>
+        </tr> */}
         </table>
       </div>
     </>
